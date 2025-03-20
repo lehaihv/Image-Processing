dff5b5da185450213c8c4505052caedca0be5f1c
lehaihv <69892340+lehaihv@users.noreply.github.com>
2025-03-20 16:49:30 -0400

    Update ocr.py

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -2,9 +2,10 @@ from PIL import Image
 import pytesseract
 
 # Load an image
-img = Image.open('p2.jpg')
+img = Image.open('p1.png')
 
 # Extract text from image
-text = pytesseract.image_to_string(img)
+#text = pytesseract.image_to_string(img)
+text = pytesseract.image_to_boxes(img)
 
 print(text)
